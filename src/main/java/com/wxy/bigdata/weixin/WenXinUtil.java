7415bf1f89d0233fcf6dc6cf9a929a37c7c4ba2b
WangYN <18787141931@163.com>
2018-12-05 15:46:16 +0800

    springboot 架构搭建

---
+++ src/main/java/com/wxy/bigdata/weixin/WenXinUtil.java
@@ -0,0 +1,13 @@
+package com.wxy.bigdata.weixin;
+
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User: WangYN
+ * Date: 2018/12/5
+ * Time: 15:44
+ * To change this template use File | Settings | File Templates.
+ * Version: V1.0
+ */
+public class WenXinUtil {
+}
